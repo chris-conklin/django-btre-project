c6d8c80bcb9a14f3d823d0171ebb97b690872030
chris-conklin <cconklin2376@gmail.com>
2020-01-15 15:17:10 +0000

    integrated templates for index and about

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     # we want the path to point to the root
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
 ]
